7e71c6411ebe1e8c5d34e74e8e57220259bf88a2
dongeun kim <deyn0629@naver.com>
2023-07-24 13:28:25 +0900

    memo_delete/modify

--- JS/Get_nutrition.js
+++ JS/Get_nutrition.js
@@ -3,7 +3,6 @@ let have_carbohydrate = "";
 let have_protein = "";
 let have_fat = "";
 
-
 let have_breakfast = {
   kcal: 0,
   carbohydrate: 0,
@@ -27,13 +26,13 @@ let have_snack = {
   carbohydrate: 0,
   protein: 0,
   fat: 0,
+};
 
 let breakfast = {
   kcal: "",
   carbohydrate: "",
   protein: "",
   fat: "",
-
 };
 
 const NUTRIAPIKEY = "052ddfc6009045afa54e";
@@ -71,9 +70,7 @@ $(".food_submit").click(function (e) {
   let food_name = $(".food_input").val();
   let food_count = $(".food_input_count").val();
 
-
   //console.log(food_info.NUTR_CONT1 * food_count);
-
 
   if (!food_info) {
     console.log("break");
@@ -91,7 +88,6 @@ $(".food_submit").click(function (e) {
   Circle_Progress_Bar();
 
   add_kcal(food_info, food_count, e);
-
 });
 
 function lower_food_info(food_info) {
@@ -308,7 +304,6 @@ function Clear_nutrition() {
   total_kcal = 0;
 }
 
-
 function add_kcal(food_info, food_count, e) {
   // 아침-----------------------------------------------------------
   if (e.target.name == "morning") {
@@ -388,4 +383,3 @@ function add_kcal(food_info, food_count, e) {
       .text(`지방:${Math.round(have_snack.fat)}`);
   }
 }
-
